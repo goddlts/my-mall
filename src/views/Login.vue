93c0d6f65471a7b40f77cbecc33c7c675f5ac791
教瘦 <54337467@qq.com>
2020-11-26 16:15:25 +0800

    登录功能-登录界面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    Login
+  <div class="login-wrap">
+    <el-form label-position="top" class="login-form" :model="form" label-width="80px">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-button" type="primary">登  录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  /* 设置该div中的内容居中显示 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-wrap .login-form {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+.login-wrap .login-form .login-button {
+  width: 100%;
+}
 </style>
